f4187cbd8176878a6312578510c5b41e2ee880fe
Rickard von Haugwitz <rickardvh@gmail.com>
2024-02-20 21:11:20 +0100

    remove unnecessary stuff

--- tests/Majako.Collections.RadixTree.Tests/TestUtils.fs
+++ tests/Majako.Collections.RadixTree.Tests/TestUtils.fs
@@ -6,17 +6,10 @@ open Swensen.Unquote.Assertions
 /// Assertion comparing two sequences for equality, ignoring order
 let inline (==!) xs ys = (Seq.sort xs |> Seq.toList) =! (Seq.sort ys |> Seq.toList)
 
-let rec eq xs ys =
-    match xs, ys with
-    | [], [] -> true
-    | _::_, [] -> false
-    | [], _::_ -> false
-    | x::xs', y::ys' -> x = y && eq xs' ys'
-
 let inline (==) xs ys =
     let xs' = Seq.sort xs |> Seq.toList
     let ys' = Seq.sort ys |> Seq.toList
-    eq xs' ys'
+    xs' = ys'
 
 /// Generalised startsWith function for sequences; motivated by a bug where string.StartsWith trims
 /// any control characters from both strings, which causes FsCheck tests to fail when they shouldn't
